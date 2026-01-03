ee0ac718bd4e6711a2bcf45c42bf4e4d2f90e626
VaibhavMurali <z5589018@ad.unsw.edu.au>
2026-01-03 22:17:23 +1100

    camFeed takes a picture with the appropriate camera using OpenCV and saves the images in the images folder. This program quits when the letter q is pressed on the keyboard

---
+++ CV_cam.py
@@ -0,0 +1,24 @@
+# This program will take pictures from the webcam that will then be used by the algorithm to determine obstacle from landscape
+# import numpy as np
+
+import cv2
+import time
+import os
+
+def camFeed():
+  cap = cv2.VideoCapture(1)
+  ret, frame = cap.read()
+
+  foldername = "images"
+  os.makedirs(foldername, exist_ok=True)
+
+  cv2.imshow("Camera Feed", frame)
+  filename = f"photo_{int(time.time())}.jpg"
+  fullPath = os.path.join(foldername, filename)
+  cv2.imwrite(fullPath, frame)
+  if cv2.waitKey(300000) & 0xFF == ord('q') :
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+  camFeed()
